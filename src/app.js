1d3873d90e06d78857ce79209b2996974d10a9c9
jessharai <129955423+jessharai@users.noreply.github.com>
2023-07-17 18:31:56 +0200

    updated date and time

--- src/app.js
+++ src/app.js
@@ -1,6 +1,32 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   console.log(response.data.condition.description);
+
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
@@ -15,10 +41,13 @@ function displayTemperature(response) {
 
   let windSpeedElement = document.querySelector("#windSpeed");
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
+
 let apiKey = "eafeeoc904c0230fca8497e0fbadtfbd";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Harare&key=eafeeoc904c0230fca8497e0fbadtfbd&units=metric";
 console.log(apiUrl);
-
 axios.get(apiUrl).then(displayTemperature);
