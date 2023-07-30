2582688234f196fb70a6e983887507f4da77b948
jessharai <129955423+jessharai@users.noreply.github.com>
2023-07-30 21:53:54 +0200

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -23,29 +23,45 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class ="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col">
-            <div class="forecast-date"> ${day}</div>
+            <div class="forecast-date"> ${formatDay(forecastDay.dt)}</div>
+            
             <img
-              src="https://openweathermap.org/img/wn/01d@2x.png"
+            src="https://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
               alt=""
               width="42"
             />
             <div class="forecast-temperature">
-              <span class="forecast-temp-max"> 22°C</span>
-              <span class="forecast-temp-min"> 10°C</span>
+              <span class="forecast-temp-max"> ${Math.round(
+                forecastDay.temp.max
+              )} °C</span>
+              <span class="forecast-temp-min"> ${Math.round(
+                forecastDay.temp.min
+              )}°C</span>
             </div>
                   </div>
         `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -129,4 +145,4 @@ fahrenheitClick.addEventListener("click", displayFahrenheitTemp);
 let celsiusClick = document.querySelector("#celsius-click");
 celsiusClick.addEventListener("click", displaycelsiusTemp);
 
-cityInput("Harare");
+cityInput("Mutare");
